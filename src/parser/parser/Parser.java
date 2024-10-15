dff183ff1c3d1960fcb6fb8f3c715c986fc338c0
Lemuel Guevara <lemueljohn.guevara@gmail.com>
2024-10-16 03:46:48 +0800

    feat(parser): added some new methods

--- src/parser/parser/Parser.java
+++ src/parser/parser/Parser.java
@@ -2,6 +2,7 @@ package parser.parser;
 
 import parser.lexer.Lexer;
 import parser.lexer.Token;
+import parser.lexer.TokenType;
 
 public abstract class Parser {
     protected Lexer lexer;
@@ -12,16 +13,23 @@ public abstract class Parser {
         this.currentToken = lexer.getNextToken();
     }
 
-    // Method to fetch the next token
-    protected Token getNextToken() {
+    protected void analyzeNextToken(TokenType expectedType) {
+        if (currentToken.getType() != expectedType) System.out.println("Invalid token type");
         currentToken = lexer.getNextToken();
-        return currentToken;
     }
 
     // Method to reset the lexer
     protected void resetLexer() {
         lexer.reset();
         currentToken = lexer.getNextToken();
+    }
+
+    protected boolean hasSemicolon() {
+        if (currentToken.getType().equals(TokenType.SEMICOLON)) {
+            analyzeNextToken(currentToken.getType());
+            return true;
+        }
+        return false;
     }
 
     // Method to check for specific tokens or conditions, can be extended further
